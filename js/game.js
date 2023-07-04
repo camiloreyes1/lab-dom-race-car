959a79eab2d40cc23e18e64aa6be9135579dd767
camiloreyes1 <jreye236@fiu.edu>
2023-07-04 18:19:46 -0400

    race car

--- js/game.js
+++ js/game.js
@@ -1,3 +1,66 @@
 class Game {
-    // code to be added
+    constructor () {
+        this.startScreen = document.getElementById("game-intro");
+        this.gameScreen = document.getElementById("game-screen");
+        this.gameEndScreen == document.getElementById("game-end");
+        this.player = new Player(this.gameScreen, 200, 500, 100, 150, "./images/car.png");        
+        this.height = "600px";
+        this.width = "500px";
+        this.obstacles = [];
+        this.counter = 0;
+        this.score = 0;
+        this.live = 3;
+        this.gameIsOver = false;
+    }
+        start () {
+
+            this.gameScreen.style.height = this.height;
+            this.gameScreen.style.width = this.width;
+            this.startScreen.style.display = "none";
+            this.startScreen.style.height = "0px"
+            this.gameScreen.style.display = "block"
+            
+            this.gameLoop()
+        }
+
+        gameLoop() {
+            if (this.gameIsOver){
+                return;
+            }
+
+            this.counter++
+            if(this.counter % 120 === 0) {
+                this.obstacles.push(new Obstacle(this.gameScreen))
+            }
+
+            this.update()
+
+            window.requestAnimationFrame(() => this.gameLoop())
+        }
+
+        update () {
+            for (let i = 0; i < this.obstacles.length; i++) {
+
+                console.log(this.obstacles[i])
+                this.obstacles[i].move()
+                
+                if (this.obstacles[i].top > 1200) {
+                    this.obstacles[i].element.remove();
+                    this.obstacles.splice(i, 1)
+                }
+                
+                if (this.player.didCollide(this.obstacles[i])) {
+                    this.obstacles[i].element.remove();
+                    this.obstacles.splice(i, 1)
+                    this.lives--
+                }
+                
+            }
+            
+            this.player.move()
+    
+            if (this.lives <= 0) {
+                this.endGame()
+            }
+        }
 }
--- js/script.js
+++ js/script.js
@@ -7,6 +7,24 @@ window.onload = function () {
   });
 
   function startGame() {
+    let game = new Game()
+    game.start()
     console.log("start game");
+
+    document.onkeydown = ((e) => {
+      if (e.key === "ArrowLeft") {
+        console.log("MovingLeft")
+        game.player.directionX -= .5
+      }
+      if (e.key === "ArrowRight") {
+        game.player.directionX += .5
+      }
+      if (e.key === "ArrowUp") {
+        game.player.directionY -= .5
+      }
+      if (e.key === "ArrowDown") {
+        game.player.directionY += .5
+      }
+    })
   }
 };
